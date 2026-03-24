833582f7d1f3fbd0948eaa2d9fa13bdeed6f9651
holodtmsk <holod.tmsk@gmail.com>
2026-03-24 22:41:07 +0700

    restore index template

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def index():
-    return "<h1>DEPLOY OK</h1>"
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 DB_PATH = "data/sbt.db"
 PRICE_LIST_PATH = "data/pricelist.txt"
